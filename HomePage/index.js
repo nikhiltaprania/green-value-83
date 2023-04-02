775ba42a8bb3b886b70a77cd5305d64395f13b3c
Nikhil Kumar <nikhiltaprania@gmail.com>
2023-04-02 21:26:54 +0530

    Day_2 Tasks Are Completed

---
+++ HomePage/index.js
@@ -0,0 +1,210 @@
+const Sale = document.getElementById('Sale');
+const New_in = document.getElementById('New_in');
+const Clothing = document.getElementById("Clothing");
+const Shoes = document.getElementById("Shoes");
+const Brands = document.getElementById("Brands");
+const Summer = document.getElementById("Summer");
+const Tailoring = document.getElementById("Tailoring")
+const Accessories = document.getElementById('Accessories');
+const Jeans = document.getElementById("Jeans");
+const Activewear = document.getElementById("Activewear");
+const Topman = document.getElementById("Topman");
+const Marketplace = document.getElementById("Marketplace");
+const Outlet = document.getElementById("Outlet");
+
+const hover_navbar_items = document.getElementById('hover_navbar_items');
+
+// for Sale
+Sale.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Sale.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+// for New_in
+New_in.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+New_in.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+
+// for Clothing
+Clothing.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Clothing.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+// for Shoes
+Shoes.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Shoes.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+// for Brands
+Brands.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Brands.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+// for Summer
+Summer.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Summer.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+// for Tailoring
+Tailoring.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Tailoring.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+// for Accessories
+Accessories.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Accessories.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+// for jeans
+Jeans.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Jeans.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+// for "Activewear"
+Activewear.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Activewear.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+// for "Topman"
+Topman.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Topman.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+// for "Marketplace"
+Marketplace.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Marketplace.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+
+// for "Outle"
+Outlet.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex';
+})
+hover_navbar_items.addEventListener('mouseover', () => {
+    hover_navbar_items.style.display = 'flex'
+
+})
+hover_navbar_items.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
+Outlet.addEventListener('mouseout', () => {
+    hover_navbar_items.style.display = 'none';
+})
